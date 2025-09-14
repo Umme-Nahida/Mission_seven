18145a839de158e66576aebe9356a445f68a7bb1
Developer Nahida <ummenahida20@gmail.com>
2025-09-14 22:12:43 +0600

    create user delete api

--- next-blog-starter/src/modules/user/user.service.ts
+++ next-blog-starter/src/modules/user/user.service.ts
@@ -1,7 +1,8 @@
+import { Prisma, User } from "@prisma/client"
 import { prisma } from "../../config/db"
 
 
-const createUser = async(payload:any)=>{
+const createUser = async(payload:Prisma.UserCreateInput):Promise<User>=>{
 
   const user = await prisma.user.create({
       data: payload
@@ -11,6 +12,85 @@ const createUser = async(payload:any)=>{
 }
 
 
+const getAllUsers = async()=>{
+
+  const users = await prisma.user.findMany({
+  select: {
+    id: true,
+    name: true,
+    email: true,
+    phone: true,
+    role: true,
+    status: true,
+    isVerified: true,
+    createdAt: true,
+    updatedAt: true,
+    address:true,
+    picture:true
+  },
+  orderBy:{
+    createdAt:"desc"
+  }
+})
+
+  return users
+}
+
+const getUser = async(id:number)=>{
+
+  const user = await prisma.user.findUnique({
+  where: {
+    id
+  },
+  select: {
+    id: true,
+    name: true,
+    email: true,
+    phone: true,
+    role: true,
+    status: true,
+    isVerified: true,
+    createdAt: true,
+    updatedAt: true,
+    address:true,
+    picture:true
+  }
+})
+
+  return user
+}
+
+// update user api
+const updateUser = async(id:number,updatInfo:any)=>{
+
+  const userUpdated = await prisma.user.updateMany({
+  where: {
+    id
+  },
+  data: updatInfo
+})
+
+  return userUpdated
+}
+
+
+// update user api
+const deleteUser = async(id:number)=>{
+
+  const user = await prisma.user.delete({
+  where: {
+    id
+  }
+})
+
+  return user
+}
+
+
 export const userService = {
-    createUser
+    createUser,
+    getAllUsers,
+    getUser,
+    updateUser,
+    deleteUser
 }
