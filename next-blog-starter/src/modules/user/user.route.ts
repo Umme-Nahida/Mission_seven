c7a27cec1300aa977600653ebc48631c3da95899
Developer Nahida <ummenahida20@gmail.com>
2025-09-14 22:11:46 +0600

    create user route and added all routes

--- next-blog-starter/src/modules/user/user.route.ts
+++ next-blog-starter/src/modules/user/user.route.ts
@@ -5,6 +5,10 @@ import { userController } from "./user.controller";
 
 const router = express.Router();
 
-router.post('/',userController.createUser)
+router.post('/create/',userController.createUser)
+router.get('/',userController.getAllUsers)
+router.get('/me/:id',userController.getUserById)
+router.patch('/update/:id',userController.updateUser)
+router.patch('/delete/:id',userController.deleteUser)
 
 export const userRouter = router;
