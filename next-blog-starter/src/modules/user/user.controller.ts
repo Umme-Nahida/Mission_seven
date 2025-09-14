62c16fb1d9d85c4320f25c9dc7dfe0baa1e91c2f
Developer Nahida <ummenahida20@gmail.com>
2025-09-14 22:11:25 +0600

    create user controller

--- next-blog-starter/src/modules/user/user.controller.ts
+++ next-blog-starter/src/modules/user/user.controller.ts
@@ -10,7 +10,53 @@ const createUser = async (req: Request, res: Response) => {
     }
 }
 
+const getAllUsers = async (req: Request, res: Response) => {
+    try {
+        const result = await userService.getAllUsers()
+        res.send(result)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const getUserById = async (req: Request, res: Response) => {
+    try {
+        const result = await userService.getUser(Number(req.params.id))
+        res.send(result)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const updateUser = async (req: Request, res: Response) => {
+    try {
+
+        const id = Number(req.params.id);
+        const updateInfo = req.body;
+        const result = await userService.updateUser(id,updateInfo)
+        res.status(201).send(result)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+const deleteUser = async (req: Request, res: Response) => {
+    try {
+
+        const id = Number(req.params.id);
+        const result = await userService.deleteUser(id)
+        res.status(201).send(result)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
 
 export const userController = {
-    createUser
+    createUser,
+    getAllUsers,
+    getUserById,
+    updateUser,
+    deleteUser
 }
