ef0bfa7d4d845c9a772dfaffb9264119454bf528
Developer Nahida <ummenahida20@gmail.com>
2025-09-14 22:10:13 +0600

    create post api to get all post data

---
+++ next-blog-starter/src/modules/post/post.controller.ts
@@ -0,0 +1,64 @@
+import { Request, Response } from "express"
+import { postService } from "./post.service"
+
+const createPost = async (req: Request, res: Response) => {
+    try {
+        const result = await postService.createPost(req.body)
+        res.send(result)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+const getAllPost = async (req: Request, res: Response) => {
+    try {
+        const result = await postService.getAllPost()
+        res.send(result)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+// get single post 
+const getPost = async (req: Request, res: Response) => {
+    try {
+        const result = await postService.getPost(Number(req.params.id))
+        res.send(result)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+// get single post 
+const updatePost = async (req: Request, res: Response) => {
+    try {
+        const id = Number(req.params.id)
+        const updateInfo = req.body;
+        const result = await postService.updatePost(id,updateInfo)
+        res.send(result)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+//delete single post in database
+const deletePost = async (req: Request, res: Response) => {
+    try {
+        const id = Number(req.params.id)
+        const result = await postService.deletePost(id)
+        res.send(result)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export const postController = {
+    createPost,
+    getAllPost,
+    getPost,
+    deletePost,
+    updatePost
+}
