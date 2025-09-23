613e512d418d0de783bad77051b013d3b8565630
Developer Nahida <ummenahida20@gmail.com>
2025-09-23 23:41:09 +0600

    added new frontend ui

--- nextjs-test-project/src/app/(commonLayout)/page.tsx
+++ nextjs-test-project/src/app/(commonLayout)/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 
 
@@ -7,16 +8,19 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter()
 
-  const handlechange = ()=>{
+  const handlechange = () => {
     router.push('/dashboard')
   }
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-     
+
       <h1 className="text-5xl">welcome to this website</h1>
-      
-            <button onClick={handlechange}>dashboard</button>
+
+      <div>
+        <button onClick={handlechange}>dashboard</button>
+        <Button>Click me</Button>
+      </div>
     </div>
   );
 }
